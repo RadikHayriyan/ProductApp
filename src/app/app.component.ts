2405e96b09c1ecd13f211e7e5a15f78469735a83
radikhayriyan <radik.h@theprojectdrivers.com>
2021-02-20 19:17:09 +0400

    product app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductService } from './app.service';
+import { cloneDeep } from 'lodash';
+import { Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'products-app';
+export class AppComponent implements OnInit, OnDestroy {
+  rows: any[];
+  clonedRows: any[];
+  nameControl = new FormControl();
+  categoryControl = new FormControl();
+  nameSub: Subscription;
+  categorySub: Subscription;
+  previousNameFilter = '';
+  previousProducerFilter = '';
+  previousCategoryFilter = '';
+  columns = [
+    { name: 'Name', prop: 'name', width: 250 },
+    { name: 'Producer', prop: 'producer', width: 250 },
+    { name: 'Category', prop: 'category', width: 250 }
+  ];
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((res: any[]) => {
+      this.rows = res;
+      this.clonedRows = cloneDeep(this.rows);
+    })
+
+    this.nameSub = this.nameControl.valueChanges.pipe(
+      debounceTime(400),
+    ).subscribe((name: string) => {
+      this.previousNameFilter = name;
+      this.rows = this.filterRows(name, this.previousProducerFilter, this.previousCategoryFilter);
+    })
+
+    this.categorySub = this.categoryControl.valueChanges.pipe(
+      debounceTime(400),
+    ).subscribe((category: string) => {
+      this.previousCategoryFilter = category;
+      this.rows = this.filterRows(this.previousNameFilter, this.previousProducerFilter, category);
+    })
+  }
+
+  filterByProducer(producer: string) {
+    this.previousProducerFilter = producer;
+    this.rows = this.filterRows(this.previousNameFilter, producer, this.previousCategoryFilter);
+  }
+
+  filterRows(nameFilter = '', producerFilter = '', categoryFilter = ''): any[] {
+    nameFilter = nameFilter.toUpperCase();
+    producerFilter = producerFilter.toUpperCase();
+    categoryFilter = categoryFilter.toUpperCase();
+
+    return this.clonedRows.filter(row => {
+      const isPartialNameMatch = row.name.toUpperCase().indexOf(nameFilter) !== -1 || !nameFilter;
+      const isPartialProducerMatch = row.producer.toUpperCase().indexOf(producerFilter) !== -1 || !producerFilter;
+      const isPartialCategoryMatch = row.category.toUpperCase().indexOf(categoryFilter) !== -1 || !categoryFilter;
+      return isPartialNameMatch && isPartialProducerMatch && isPartialCategoryMatch;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.nameSub.unsubscribe();
+    this.categorySub.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { FormsModule }   from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductService } from './app.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxDatatableModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
